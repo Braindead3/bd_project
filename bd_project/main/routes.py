63185b7780a1cfb51ca2b178df656c46bbbab653
Braindead3 <malosay21@gmail.com>
2022-01-27 19:44:52 +0300

    add categories filtration

--- bd_project/main/routes.py
+++ bd_project/main/routes.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import render_template, Blueprint, flash,redirect,url_for
+from flask import render_template, Blueprint, flash, redirect, url_for
 from flask_login import current_user
 from playhouse.flask_utils import object_list
 
@@ -10,26 +10,50 @@ from bd_project.models import Product
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
-@main.route('/home', methods=['GET', 'POST'])
-def home():
+def choose_categories():
+    all_products = Product.select()
+    product_categories = set()
+    for product in all_products:
+        product_categories.add(product.category)
+    return list(product_categories)
+
+
+def current_user_ordered_products():
     order_products_by_current_user = None
-    form = SearchForm()
     if current_user.is_authenticated:
         with open('ordered_products.json', 'r') as f:
             order_products_by_users = json.load(f)
             order_products_by_current_user = order_products_by_users.get(f'{current_user.id}')
+    return order_products_by_current_user
+
+
+@main.route('/', methods=['GET', 'POST'])
+@main.route('/home', methods=['GET', 'POST'])
+def home():
+    product_categories = choose_categories()
+    form = SearchForm()
+    order_products_by_current_user = current_user_ordered_products()
     if form.validate_on_submit():
-        products = Product.select().where(Product.name.startswith(f'{form.search_product_name.data}'))
+        products = Product.select().where(Product.name.contains(f'{form.search_product_name.data.capitalize()}'))
         if not products:
             flash('Nothing found', 'danger')
             products = Product.select()
     else:
         products = Product.select()
-    return object_list('home.html', title='Home page', query=products, context_variable='products', paginate_by=2,
-                       order_products=order_products_by_current_user, form=form)
+    return object_list('home.html', title='Home page', query=products, context_variable='products', paginate_by=10,
+                       order_products=order_products_by_current_user, form=form, categories=product_categories)
 
 
 @main.route('/admin_tables')
 def admin_page():
     return render_template('admin_pages/admin.html', title='admin_users_table')
+
+
+@main.route('/filtered_products/<category>')
+def filter_products_by_category(category):
+    product_categories = choose_categories()
+    form = SearchForm()
+    products = Product.select().where(Product.category == category)
+    order_products_by_current_user = current_user_ordered_products()
+    return object_list('home.html', title='Home page', query=products, context_variable='products', paginate_by=10,
+                       order_products=order_products_by_current_user, form=form, categories=product_categories)
