460ee18bdbca756638b50a16d5cb5f4def3d09ac
Braindead3 <malosay21@gmail.com>
2022-02-10 01:44:19 +0300

    config mail

--- bd_project/config.py
+++ bd_project/config.py
@@ -3,8 +3,9 @@ import os
 
 class Config:
     SECRET_KEY = '3ea4895512377c1df5cd9b54ea5ef09d'
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = 'vladbloghelper@gmail.com'
     MAIL_PASSWORD = 'Vl060272'
